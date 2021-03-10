cab0b8221ee678ad9a95b288fda7c2874f680851
Martin Simonini <soy_martin98@yahoo.com>
2021-03-10 14:40:37 -0600

    Moved AWC endpoints to their own controller class.

---
+++ src/main/java/edu/unomaha/flightriskassessment/controller/AwcController.java
@@ -0,0 +1,59 @@
+package edu.unomaha.flightriskassessment.controller;
+
+import edu.unomaha.flightriskassessment.models.*;
+import edu.unomaha.flightriskassessment.services.AWCServices;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class AwcController
+{
+    private static final Logger              logger = LogManager.getLogger(AwcController.class);
+
+    @Autowired
+    private AWCServices awcServices = new AWCServices();
+
+    @GetMapping(path="/getMetar")
+    public @ResponseBody
+    Metar getMetarData(String airportID)
+    {
+        logger.info("Beginning getMetarData request function...");
+        return awcServices.getMetarData(airportID);
+    }
+
+    @GetMapping(path="/getTaf")
+    public @ResponseBody
+    Taf getTafData(@RequestParam int radius, @RequestParam String latLong)
+    {
+        logger.info("Beginning getTafData...");
+        return awcServices.getTafData(radius, latLong);
+
+    }
+
+    @GetMapping(path="/getAirSigmet")
+    public @ResponseBody
+    List<AirSigmet> getAirSigmet()
+    {
+        logger.info("Beginning getAirSigmet...");
+        return awcServices.getAirSigmet();
+
+    }
+
+    @GetMapping(path="/getPireps")
+    public @ResponseBody
+    List<Pirep> getPireps(@RequestParam int radius, @RequestParam String latLong)
+    {
+        logger.info("Beginning getPirep request...");
+        return awcServices.getPireps(radius,latLong);
+
+    }
+
+}
--- src/main/java/edu/unomaha/flightriskassessment/controller/MainController.java
+++ src/main/java/edu/unomaha/flightriskassessment/controller/MainController.java
@@ -2,18 +2,15 @@ package edu.unomaha.flightriskassessment.controller;
 
 import edu.unomaha.flightriskassessment.database.ProfessorRepository;
 import edu.unomaha.flightriskassessment.models.*;
-import edu.unomaha.flightriskassessment.services.AWCServices;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
 
 
 @Controller
@@ -22,9 +19,6 @@ public class MainController
     private static final Logger logger = LogManager.getLogger(MainController.class);
     @Autowired
     private ProfessorRepository professorRepository;
-
-    @Autowired
-    private AWCServices awcServices = new AWCServices();
 
     @PostMapping(path="/addNewProfessor")
     public @ResponseBody String addNewUser (@RequestParam String firstName, @RequestParam String lastName,
@@ -42,42 +36,5 @@ public class MainController
     public @ResponseBody Iterable<Professor> getAllProfessors() {
         return professorRepository.findAll();
     }
-
-    @GetMapping(path="/getMetar")
-    public @ResponseBody Metar getMetarData(String airportID)
-    {
-        logger.info("Beginning getMetarData request function...");
-        Metar metar = awcServices.getMetarData(airportID);
-        return metar;
-    }
-
-    @GetMapping(path="/getTaf")
-    public @ResponseBody
-    Taf getTafData(@RequestParam int radius, @RequestParam String latLong)
-    {
-        logger.info("Beginning getTafData...");
-         Taf taf = awcServices.getTafData(radius, latLong);
-         return taf;
-
-    }
-
-    @GetMapping(path="/getAirSigmet")
-    public @ResponseBody
-    List<AirSigmet> getAirSigmet()
-    {
-        logger.info("Beginning getAirSigmet...");
-        return awcServices.getAirSigmet();
-
-    }
-
-    @GetMapping(path="/getPireps")
-    public @ResponseBody
-    List<Pirep> getPireps(@RequestParam int radius, @RequestParam String latLong)
-    {
-        logger.info("Beginning getPirep request...");
-        return awcServices.getPireps(radius,latLong);
-
-    }
-
 
 }
